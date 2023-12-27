7b15a0d2971d1e670afc71826a7e3cc2c81c1083
patoamaya <patriamayaford@gmail.com>
2023-12-27 02:02:28 -0300

    final clase 13

--- src/components/FormCheckout/FormCheckout.jsx
+++ src/components/FormCheckout/FormCheckout.jsx
@@ -1,33 +1,62 @@
-import { TextField, Grid } from '@mui/material'
+import { TextField, Grid, Button } from '@mui/material'
 import React from 'react'
 import "./FormCheckout.css"
 
-const FormCheckout = () => {
+const FormCheckout = ({handleChange, handleSubmit, errors}) => {
   return (
     <div>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
       <Grid container spacing={0} className='gridContainer'>
           <Grid item  xs={10} md={8} >
           <TextField id="outlined-basic"
            label="Nombre"
             variant="outlined"
-            fullWidth />
+            name='nombre'
+            fullWidth
+            error={errors.nombre ? true : false}
+            helperText={errors.nombre}
+            onChange={handleChange}
+           />
           </Grid>
-          <Grid xs={10} md={8}>
+          <Grid item xs={10} md={8}>
           <TextField
            id="outlined-basic"
-            label="Apellido"
+            label="Email"
             variant="outlined"
+            name='email'
+            error={errors.email ? true : false}
+            helperText={errors.email}
+            onChange={handleChange}
              fullWidth/>
           </Grid>
-          <Grid xs={10} md={8}>
+          <Grid item xs={10} md={8}>
           <TextField 
           id="outlined-basic" 
           label="Contraseña"
            variant="outlined" 
           type='password'
+          name='contraseña'
+          error={errors.contraseña ? true : false}
+          helperText={errors.contraseña}
+          onChange={handleChange}
+           fullWidth/>
+          <TextField 
+          id="outlined-basic" 
+          label="Confirmar Contraseña"
+           variant="outlined" 
+          type='password'
+          name='confirmContraseña'
+          error={errors.confirmContraseña ? true : false}
+          helperText={errors.confirmContraseña}
+          onChange={handleChange}
            fullWidth/>
           </Grid>
+          <Grid item xs={10} md={8} >
+          <Button color="primary" variant="contained" className='btnCompra' type='submit' >
+            Comprar            
+          </Button>
+          </Grid>
+           
 
       </Grid>
         </form>
--- src/components/FormCheckout/FormCheckoutContainer.jsx
+++ src/components/FormCheckout/FormCheckoutContainer.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import FormCheckout from './FormCheckout'
-
+import {useFormik} from "formik"
+import * as Yup from "yup"
 
 const FormCheckoutContainer = () => {
+
+const {handleChange, handleSubmit, errors} = useFormik({
+    initialValues:{
+      nombre: "",
+      email: "",
+      contraseña:"",
+      confirmContraseña:""
+    },
+    onSubmit: (data)=>{
+      console.log(data)
+      
+    },
+    validationSchema: Yup.object().shape({
+      nombre: Yup.string().required("Este campo es obligatorio").min(3, "La cantidad de carácteres mínima es 3"),
+      email: Yup.string().email("Debe proporcionar un Email válido").required("Este campo es obligatiorio"),
+      contraseña: Yup.string().required("Este campo es obligatorio")
+      .matches(/^(?=.*[a-z])(?=.*\d)(?=.*[A-Z]).{6,15}$/, {
+        message:"La contraseña debe tener al menos 1 mayuscula, 1 minuscula, 1 numero y contener entre 6 y 15 caracteres"
+    }),
+      confirmContraseña: Yup.string().required("Este campo es obligatorio")
+      .oneOf([Yup.ref("contraseña")], "Las contraseñas deben coincidir")
+      }),
+    validateOnChange: false,
+})
+console.log(errors)
+
   return (
-    <div><FormCheckout/></div>
+    <div><FormCheckout handleChange={handleChange} handleSubmit={handleSubmit} errors = {errors}/></div>
   )
 }
 
